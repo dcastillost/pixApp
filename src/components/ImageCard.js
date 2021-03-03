cc34879f2fa9ad97202cd73288280c31a576f44c
David Castillo <dcastillo.st@gmail.com>
2021-03-03 20:05:46 +0900

    Complete functionality

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,20 +4,29 @@ class ImageCard extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = { spans: 0 };
     this.imageRef = React.createRef();
   }
 
   componentDidMount() {
-    console.log(this.imageRef);
-    console.log(this.imageRef.current.clientHeight);
+    // this.imageRef.current.addEventListener('load', this.setSpans);
   }
+
+  setSpans = () => { //Define as arrow function for binding of 'this'
+    // console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+
+    // this.setState({ spans: spans });
+    this.setState({ spans });
+  };
 
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img alt={description} ref={this.imageRef} src={urls.regular} onLoad={this.setSpans} />
       </div>
     );
   }
